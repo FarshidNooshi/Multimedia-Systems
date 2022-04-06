6ac89a9d88bcd8446f54e8aece4ec826bcf455b8
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-04-06 16:18:58 +0430

    first section is implementetd

--- assignment 2/src/main.py
+++ assignment 2/src/main.py
@@ -1,5 +1,18 @@
 import numpy as np
-import matplotlib as mpl
+from PIL import Image
+from matplotlib import pyplot as plt
 
 
-print(np.array(2))
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.299, 0.5870, 0.1140])
+
+
+# address = input('please enter address of image: ')
+address = '/Volumes/Farshid_SSD/Projects/University/MultiMediaSystems/assignment 2/assets/image.png'
+image = Image.open(address)
+print(image.mode)
+
+im_arr = np.array(image)
+
+plt.imshow(rgb2gray(im_arr), cmap='gray', vmin=0, vmax=255)
+plt.show()
