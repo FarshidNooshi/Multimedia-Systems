5fa450111cdeea0254fe464417409dc5b3c94a8f
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-04-07 03:21:20 +0430

    second section got implemented

--- assignment 2/src/main.py
+++ assignment 2/src/main.py
@@ -1,3 +1,5 @@
+import collections
+
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
@@ -7,12 +9,28 @@ def rgb2gray(rgb):
     return np.dot(rgb[..., :3], [0.299, 0.5870, 0.1140])
 
 
-# address = input('please enter address of image: ')
-address = '/Volumes/Farshid_SSD/Projects/University/MultiMediaSystems/assignment 2/assets/image.png'
-image = Image.open(address)
-print(image.mode)
+def image_read():
+    # address = input('please enter address of image: ')
+    address = '/Volumes/Farshid_SSD/Projects/University/MultiMediaSystems/assignment 2/assets/image.png'
+    image = Image.open(address)
+    print('for test, pixel at position(1, 25) is ' + image.getpixel((1, 25)).__str__())
+    return image
 
-im_arr = np.array(image)
 
-plt.imshow(rgb2gray(im_arr), cmap='gray', vmin=0, vmax=255)
-plt.show()
+def section_one():
+    image = image_read()
+    print('mode of initial image: ' + image.mode)
+    im_arr = np.array(image)
+    gray = rgb2gray(im_arr)
+    plt.imshow(gray, cmap='gray', vmin=0, vmax=255)
+    plt.show()
+    return gray
+
+
+def section_two(number_of_bins=40):
+    data = section_one()
+    data_1d = data.flatten()
+    plt.hist(data_1d, density=True, bins=number_of_bins)
+    plt.show()
+
+
