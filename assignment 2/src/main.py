be5e3949399d6bed5efeb7246cea5d368c80601a
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-04-07 18:46:28 +0430

    third section

--- assignment 2/src/main.py
+++ assignment 2/src/main.py
@@ -3,6 +3,7 @@ import collections
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
+from numpy import shape
 
 
 def rgb2gray(rgb):
@@ -22,6 +23,7 @@ def section_one():
     print('mode of initial image: ' + image.mode)
     im_arr = np.array(image)
     gray = rgb2gray(im_arr)
+    gray = gray.astype(int)
     plt.imshow(gray, cmap='gray', vmin=0, vmax=255)
     plt.show()
     return gray
@@ -32,5 +34,21 @@ def section_two(number_of_bins=40):
     data_1d = data.flatten()
     plt.hist(data_1d, density=True, bins=number_of_bins)
     plt.show()
+    return data
 
 
+def section_three():
+    data = section_two()
+    data_flattened = data.flatten()
+    counter = dict(sorted(collections.Counter(data_flattened).items()))
+    counter_keys = counter.keys()
+    counter_values = counter.values()
+    counter_values = [i for i in counter_values]
+    for i in range(1, len(counter_values)):
+        counter_values[i] = counter_values[i] + counter_values[i - 1]
+    counter_values = np.array(counter_values)
+    counter_keys = np.array([i for i in counter_keys])
+    return dict(zip(counter_keys, counter_values))
+
+
+print(section_three())
