1385fe292cb21273f7ee3c6cfa4743ee6574f43a
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-06-30 13:52:26 +0430

    add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MultiMediaSystems',
+    version='1',
+    packages=[''],
+    url='https://github.com/FarshidNooshi/Multi-Media-Systems',
+    license='MIT',
+    author='Farshid Nooshi',
+    author_email='farshidnooshi726@hotmail.com',
+    description='all the assignments for Multi media systems course in Computer Engineering Department at Amirkabir university of technology'
+)
